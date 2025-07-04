15ecc694c93e30c6a21d44c7891b4bc8e363119d
nangshushanna <zushanar@gmail.com>
2025-07-04 10:46:17 +0700

    9.2  and 9.3 done

--- 331-intro-to-vite-new/src/App.vue
+++ 331-intro-to-vite-new/src/App.vue
@@ -13,8 +13,7 @@ import { RouterLink, RouterView } from 'vue-router'
         </nav>
       </div>
     </header>
-    <h1>Events For Good</h1>
-    <!--new element-->
+
     <RouterView/>
   </div>
 </template>

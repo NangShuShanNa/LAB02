79566331b67946718b1de380865609d7129bbdcd
nangshushanna <zushanar@gmail.com>
2025-07-23 12:56:42 +0700

    lab3 4 is done

--- 331-intro-to-vite-new/src/views/event/RegisterView.vue
+++ 331-intro-to-vite-new/src/views/event/RegisterView.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
-
+import { useRouter } from 'vue-router'
 import { type Event } from '@/types'
 
-
+// Define props for event and id
 const props = defineProps<{
   event: Event
-    id: String
+  id: string
 }>()
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { event } = toRefs(props)
+// Extract refs from props
+const { event, id } = toRefs(props)
 
+// Create router instance
+const router = useRouter()
+
+// Register handler
+const register = () => {
+  // If the registration API call successful (simulate here)
+  // Push back to the event details view
+  router.push({ name: 'event-detail-view', params: { id: id.value } })
+}
 </script>
 
 <template>
-   <p>Register event here</p>
+  <div>
+    <p>Register event here</p>
+    <button @click="register">Register</button>
+  </div>
 </template>

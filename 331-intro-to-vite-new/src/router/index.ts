4800589d5a1e304792d9057a718c26af7fb17dc4
nangshushanna <zushanar@gmail.com>
2025-07-23 13:15:33 +0700

    lab3 5.1 done

--- 331-intro-to-vite-new/src/router/index.ts
+++ 331-intro-to-vite-new/src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
-import AboutView from '../views/AboutView.vue'
-import StudentListView from '@/views/StudentListView.vue' // Add this import
+import AboutView from '@/views/AboutView.vue'
+import StudentListView from '@/views/StudentListView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,52 +15,52 @@ const router = createRouter({
       path: '/',
       name: 'event-list-view',
       component: EventListView,
-        props: (route) => ({
-    page: parseInt(route.query.page?.toString() || '1')
-  })
-
+      props: (route) => ({
+        page: parseInt(route.query.page?.toString() || '1')
+      })
     },
     {
-  path: '/event/:id',
- name: 'event-layout-view',
-component: EventLayoutView,
-props: true,
-children: [
-  {
-    path: '',
-    name: 'event-detail-view',
-    component: EventDetailView,
-    props: true
-  },
-  {
-    path: 'register',
-    name: 'event-register-view',
-    component: EventRegisterView,
-    props: true
-  },
-  {
-    path: 'edit',
-    name: 'event-edit-view',
-    component: EventEditView,
-    props: true
-  }
-]
-
- },
-
-
+      path: '/event/:id',
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
+    },
     {
       path: '/about',
       name: 'about',
-     component:AboutView
-
+      component: AboutView
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     },
     {
       path: '/students',
       name: 'Students',
-      component: ()=> import('@/views/StudentListView.vue')
-    },
-  ],
+      component: StudentListView
+    }
+  ]
 })
 
 export default router
---
+++ 331-intro-to-vite-new/src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    resource: string
+  }>(),
+  {
+    resource: 'page'
+  }
+)
+</script>
+
+<template>
+  <div>
+    <h1>Opps!</h1>
+    <h3>The page uou are looking for is not here</h3>
+    <router-link :to="{ name: 'event-list-view' }">Back to Home Page</router-link>
+  </div>
+</template>

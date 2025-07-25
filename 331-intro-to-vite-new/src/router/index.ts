76590e9b6e7f50a2118adf34ff4e80ed2ef3865a
nangshushanna <zushanar@gmail.com>
2025-07-25 10:04:26 +0700

    lab4 5.4 is  done

--- 331-intro-to-vite-new/src/router/index.ts
+++ 331-intro-to-vite-new/src/router/index.ts
@@ -18,7 +18,7 @@ import 'nprogress/nprogress.css'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
-  // ✅ Add scroll behavior
+  // ✅ Improved scroll behavior
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
@@ -47,7 +47,6 @@ const router = createRouter({
 
         return EventService.getEvent(id)
           .then((response) => {
-            // ✅ Save the data into the store
             eventStore.setEvent(response.data)
             return true
           })

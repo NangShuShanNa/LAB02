8004ebd0657f3e7f75684f4bed7267899d572347
nangshushanna <zushanar@gmail.com>
2025-08-01 10:18:40 +0700

    lab5 7 is done

--- 331-intro-to-vite-new/tailwind.config.ts
+++ 331-intro-to-vite-new/tailwind.config.ts
@@ -10,7 +10,10 @@ export default {
     extend: {
       boxShadow:{
         'sp':'0 3px 12px 0 rgba(0, 0, 0, 0.2)'
-      }
+      },
+      fontFamily:{
+        sans: ['Avenir','Helvetica','Arail','sans-serif']
+      },
     },
   },
   plugins: [],

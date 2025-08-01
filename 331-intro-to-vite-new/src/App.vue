b4d741c915efe47c0cd08d12e3e8e280daa733c8
nangshushanna <zushanar@gmail.com>
2025-08-01 09:46:34 +0700

    lab5 3 is done

--- 331-intro-to-vite-new/src/App.vue
+++ 331-intro-to-vite-new/src/App.vue
@@ -8,7 +8,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div id="layout">
+  <div id="text-center font-sans text-gay-700 antialias">
     <header>
        <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -28,14 +28,7 @@ const { message } = storeToRefs(store)
 </template>
 
 <style >
-#layout{
-  font-family: Avenir,Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 
-}
 nav{
   padding: 30px;
 }

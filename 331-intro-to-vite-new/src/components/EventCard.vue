646c35ca4d80d9e333822ce8e95f019340204bed
nangshushanna <zushanar@gmail.com>
2025-08-01 10:13:50 +0700

    new 5 edit

--- 331-intro-to-vite-new/src/components/EventCard.vue
+++ 331-intro-to-vite-new/src/components/EventCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type Event } from '@/types'  // Ensure this path matches your project
+import { type Event } from '@/types'
 defineProps<{
   event: Event
 }>()
@@ -10,7 +10,7 @@ defineProps<{
     class="event-link"
     :to="{ name: 'event-detail-view', params: { id: event.id } }"
   >
-    <div class="event-card">
+    <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp transition">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
@@ -18,19 +18,6 @@ defineProps<{
 </template>
 
 <style scoped>
-.event-card {
-  padding: 1rem;        /* Tailwind's p-4 ≈ 16px */
-  width: 16rem;         /* Tailwind's w-64 ≈ 256px */
-  cursor: pointer;
-  border: 1px solid #4b5563;  /* Tailwind's border-gray-600 */
-  margin-bottom: 1.5rem; /* Tailwind's mb-6 ≈ 24px */
-}
-
-.event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-
 .event-link {
   text-decoration: none;
   color: #2c3e50;
